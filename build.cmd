1eb2098584b227b12c0b1ad95bab8767b3824988
Bill Tudor <bill@tudormobile.com>
2024-04-25 16:25:13 -0400

    Update build script

--- build.cmd
+++ build.cmd
@@ -7,3 +7,6 @@ if %errorlevel% neq 0 exit /b %errorlevel%
 docfx docs/docfx.json
 if %errorlevel% neq 0 exit /b %errorlevel%
 del docs\_site\*.ico
+powershell -command compress-archive -Path src\bin\release\*.nupkg -Destination package.zip
+set /p ver=<src\bin\release\ver.txt
+echo version=%ver%
