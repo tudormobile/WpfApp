b8f9eb9a6cdf7936e12d151ea203023c49f16d6f
Bill Tudor <bill@tudormobile.com>
2025-08-02 09:10:04 -0400

    Completed all open issues. Closes #12 #11 #9 #7 #6

--- build.cmd
+++ build.cmd
@@ -3,7 +3,11 @@ dotnet test --logger trx;LogFilePrefix=output --collect "Code Coverage"
 if %errorlevel% neq 0 exit /b %errorlevel%
 dotnet build -c Release
 if %errorlevel% neq 0 exit /b %errorlevel%
-rem dotnet tool update -g docfx
+dotnet tool update -g dotnet-Coverage
+dotnet-coverage merge **\*.coverage --remove-input-files --output-format xml --output output.xml
+dotnet tool update -g dotnet-reportgenerator-globaltool
+reportgenerator -reports:"output.xml" -targetdir:"output" -reporttypes:"MarkdownSummaryGithub"
+dotnet tool update -g docfx
 if %errorlevel% neq 0 exit /b %errorlevel%
 docfx docs/docfx.json
 if %errorlevel% neq 0 exit /b %errorlevel%
@@ -11,10 +15,6 @@ del docs\_site\*.ico
 powershell -command compress-archive -Path src\bin\release\*.nupkg -Destination package.zip -Force
 set /p ver=<src\bin\release\ver.txt
 echo version=%ver%
-rem dotnet tool update -g dotnet-Coverage
-dotnet-coverage merge **\*.coverage --remove-input-files --output-format xml --output output.xml
-rem dotnet tool install -g dotnet-reportgenerator-globaltool
-reportgenerator -reports:"output.xml" -targetdir:"output" -reporttypes:"MarkdownSummaryGithub"
 
 echo Test Results
 set reg=executed=\"(\d+)\" passed=\"(\d+)\" failed=\"(\d+)\"
