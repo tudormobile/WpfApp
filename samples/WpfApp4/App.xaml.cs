cc1fd9ca4b0c9108a22c6920e6ba804ef24721c9
Bill Tudor <bill@tudormobile.com>
2025-08-02 09:10:04 -0400

    Completed all open issues. Closes #12 #11 #9 #7 #6

---
+++ samples/WpfApp4/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+using Tudormobile.Wpf;
+using Tudormobile.Wpf.Commands;
+using WpfApp4.Views;
+
+namespace WpfApp4
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : WpfApplication
+    {
+        protected override void OnMainWindowCreated()
+        {
+            if (MainWindow.DataContext is MainWindowModel model)
+            {
+                model.Controls.Add(typeof(HelloWorldControl));
+            }
+        }
+
+        [Execute(nameof(MainWindowModel.SelectControlCommand))]
+        public void SelectUIItem(Type t)
+        {
+            if (MainWindow.DataContext is MainWindowModel model)
+            {
+                model.SelectedUI = App!.CreateView(t);
+            }
+        }
+    }
+
+}
