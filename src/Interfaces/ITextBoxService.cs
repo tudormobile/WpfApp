b5b5840cd56a8c085d75d9d4e2dc7e6af38b8d7a
Bill Tudor <bill@tudormobile.com>
2025-08-26 09:36:48 -0400

    Added the TextBox services.

---
+++ src/Interfaces/ITextBoxService.cs
@@ -0,0 +1,19 @@
+﻿namespace Tudormobile.Wpf;
+
+/// <summary>
+/// Service for managing TextBox related functionality. Currently supports
+/// selecting all text in a TextBox when it receives focus.
+/// <para>
+/// You must register an implementation of this interface with the service collection,
+/// and then call the Register() method in your application's OnStartup override. This
+/// is done automatically if you use the UseTextBoxService extension method in your
+/// initializtion code.
+/// </para>
+/// </summary>
+internal interface ITextBoxService
+{
+    /// <summary>
+    /// Registers and activates the service.
+    /// </summary>
+    void Register();
+}
---
+++ src/Services/TextBoxService.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace Tudormobile.Wpf.Services
+{
+    /// <inheritdoc/>
+    internal class TextBoxService : ITextBoxService
+    {
+        private bool _registered = false;
+        /// <inheritdoc/>
+        public void Register()
+        {
+            if (!_registered)
+            {
+                _registered = true;
+                EventManager.RegisterClassHandler(typeof(TextBox), TextBox.PreviewMouseLeftButtonDownEvent,
+                    new MouseButtonEventHandler(ignoreMouseButton));
+                EventManager.RegisterClassHandler(typeof(TextBox), TextBox.GotKeyboardFocusEvent,
+                    new RoutedEventHandler(selectAll));
+                EventManager.RegisterClassHandler(typeof(TextBox), TextBox.MouseDoubleClickEvent,
+                    new RoutedEventHandler(selectAll));
+            }
+        }
+
+        private void selectAll(object sender, RoutedEventArgs e) => (e.OriginalSource as TextBox)?.SelectAll();
+        private void ignoreMouseButton(object sender, MouseButtonEventArgs e)
+        {
+            // Find the TextBox
+            var parent = e.OriginalSource as DependencyObject;
+            while (parent != null && parent is not TextBox)
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            if (parent is TextBox textBox)
+            {
+                if (!textBox.IsKeyboardFocusWithin)
+                {
+                    textBox.Focus();
+                    // stop future processing of this event
+                    e.Handled = true;
+                }
+                else
+                {
+                    if (e.ClickCount > 2)
+                    {
+                        // On triple-click, select all text
+                        textBox.SelectAll();
+                    }
+                }
+            }
+        }
+    }
+}
